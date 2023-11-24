96862c2a640ded97bd922266b1f1e2989171b1c6
Christian Guetnga <lsdrfrx@gmail.com>
2023-11-24 12:39:13 +0000

    min: fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="datamoire",
-    verison="1.0.0",
+    version="1.0.0",
     description="Python library for data processing",
     url="https://github.com/lsdrfrx/datamoire",
     author="Christian Guetnga",
