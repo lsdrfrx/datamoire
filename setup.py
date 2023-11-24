6b8793487be7e846c0e755a2429dd10a8f350af9
Christian Guetnga <lsdrfrx@gmail.com>
2023-11-24 11:02:03 +0000

    min: added classifiers in setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,9 @@ setup(
     author="Christian Guetnga",
     author_email="lsdrfrx@gmail.com",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
