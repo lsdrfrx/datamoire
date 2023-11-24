8a7f8fc0b2361db85a215cf3f28b656fde84f8e2
Christian Guetnga <lsdrfrx@gmail.com>
2023-11-24 13:19:32 +0000

    min: added dependencies in setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,17 @@ setup(
     author="Christian Guetnga",
     author_email="lsdrfrx@gmail.com",
     packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchmetrics",
+        "plotly",
+        "dataclasses",
+        "statsmodels",
+        "numpy",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
 )
